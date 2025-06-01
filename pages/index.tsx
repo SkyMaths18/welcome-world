fdcb371e8037970230d9f4bcca0f8de642e48fee
SkyMaths <73053802+SkyMaths18@users.noreply.github.com>
2025-06-01 15:18:11 +0200

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Hello Vercel 🎉</h1>
-      <p>Déploiement Next.js via GitHub réussi !</p>
-    </div>
-  )
-}
